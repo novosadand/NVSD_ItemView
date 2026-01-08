365ecb6c3ba30892f5a57bbcfcbb68ebcac1c960
Andrej Novosad <novosad.and@gmail.com>
2026-01-08 19:08:02 +0100

    Auto-zoom to show item's used portion on selection
    When selecting an item, the view now zooms in to show just the portion
    the item is using (between start/end markers) with 30% padding, rather
    than showing the entire source file.
    
    If the item uses most of the source, shows the full file instead.

--- NVSD_ItemView.lua
+++ NVSD_ItemView.lua
@@ -370,12 +370,28 @@ local function loop()
           end
 
           -- Reset pan and zoom when item changes
-          if item ~= state.last_panned_item then
-            state.pan_offset = 0
+          -- Set initial zoom/pan to show the item's used portion (not entire source)
+          if item ~= state.last_zoomed_item or item ~= state.last_panned_item then
+            -- Calculate zoom to fit item portion with some padding
+            local item_portion = source_item_length
+            local padding = 1.3  -- 30% padding on each side
+            local target_view = item_portion * padding
+
+            if target_view < source_length then
+              -- Zoom in to show item portion
+              state.zoom_level = source_length / target_view
+
+              -- Center view on the item's portion
+              local item_center = start_offset + source_item_length / 2
+              local source_center = source_length / 2
+              state.pan_offset = item_center - source_center
+            else
+              -- Item covers most/all of source, just show everything
+              state.zoom_level = 1.0
+              state.pan_offset = 0
+            end
+
             state.last_panned_item = item
-          end
-          if item ~= state.last_zoomed_item then
-            state.zoom_level = 1.0
             state.last_zoomed_item = item
           end
 
